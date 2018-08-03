a5bede5d2ebd6dc5709dd1a143130d779649b294
Matheus Duarte <mfduar8766@gmail.com>
2018-08-03 18:26:40 -0400

    Finished the feature that comares genre to food.

--- moviEATS.js
+++ moviEATS.js
@@ -1,70 +1,64 @@
-var url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query='
-var url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query='
+var url =
+  "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query=";
+var url =
+  "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query=";
+
 var form = document.querySelector(".form");
-var moviesArray = [];
-var foodArray = ['hotdog','taco','pizza','icecream'];
- form.addEventListener("submit", function(e) {
+
+//We will use the key and set the value of the food.
+var foodObject = {
+  Comedy: "hotdog",
+  "Horror, Sci-Fi": "taco",
+  Drama: "icecream",
+  Adventure: "pizza",
+  "Sci-fi": "beer",
+  "Comedy, Drama": "More beer"
+};
+
+const object1 = {
+  a: "somestring",
+  b: 42,
+  c: false
+};
+
+// console.log(Object.keys(object1).includes('a'));
+
+form.addEventListener("submit", function(e) {
   e.preventDefault();
   displayContent();
 });
 
- var displayContent = function() {
+var displayContent = function() {
   var output = document.querySelector(".output");
   var mainInput = document.querySelector(".main-input");
   var cardList = document.querySelector(".card-list");
   getMovieData(mainInput, output, cardList);
 };
+
 var getMovieData = function(input, output, cardList) {
-    var input = input;
-    console.log(input);
-    var output = output;
-    console.log(output)
-    var card = cardList;
-    console.log(cardList);
-    $.ajax({
-        type: "GET",
-        url: `http://www.omdbapi.com/?apikey=24ec2260&t=${input.value}`,
-        success: function(data) {
-          moviesArray.push(data);
-          moviesArray.forEach(function(movie) {
-            if( movie.Genre === "Comedy, Drama") {
-                output.classList.add('show');
-                card.textContent = movie.Title;
+  var input = input;
+  var output = output;
+  var card = cardList;
 
-            }
-          
-          });
-        },
-        error: function(error) {
-          console.log(error);
-        }
-      });
-    };
-
-
-$.ajax(url + "pasta", {
-    headers: {
-        "X-Mashape-Key": recipeKey,
-        "Accept": "application/json" ,
-    },
-    method: "GET", 
-    success: function(data) {
-        console.log(data)
+  var filteredFood = function(movie) {
+    var keys = Object.keys(foodObject);
+    console.log(keys);
+    if (keys.includes(movie.Genre)) {
+      output.classList.add("show");
+      card.textContent = movie.Genre;
     }
-})
- var getMoviesAPI = function() {
+  };
+
   $.ajax({
-    type:'GET',
-    url:`http://www.omdbapi.com/?apikey=24ec2260&t`,
+    type: "GET",
+    url: "http://www.omdbapi.com/?apikey=24ec2260&t=" + input.value,
     success: function(data) {
+      var moviesArray = [];
       moviesArray.push(data);
-      moviesArray.forEach(function(movies){
-        return movies;
-      })
+      moviesArray.forEach(filteredFood);
     },
     error: function(error) {
       console.log(error);
     }
-  })
-}
- getMoviesAPI();
+  });
+};
