54b6fb9db8053574a3fd204f085bf8a9790261ab
TRACY S MUSIKER <tracymusiker@TRACYs-MacBook-Air.local>
2018-08-03 13:23:12 -0400

    workable get access to recipe api

---
+++ moviEATS.js
@@ -0,0 +1,17 @@
+// if comedy = chicken
+// then genrefood = chicken
+
+
+
+var url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query='
+
+$.ajax(url + "pasta", {
+    headers: {
+        "X-Mashape-Key": recipeKey,
+        "Accept": "application/json" ,
+    },
+    method: "GET", 
+    success: function(data) {
+        console.log(data)
+    }
+})
