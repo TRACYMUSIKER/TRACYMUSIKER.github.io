95e49dbf4515f99bb2aa006c05c7875cde53a948
Steven Berk <stevenberk@Stevens-MacBook-Pro.local>
2018-08-03 14:46:09 -0400

    button feature fixed

--- moviEATS.js
+++ moviEATS.js
@@ -1,3 +1,66 @@
+
+// var form = document.querySelector(".form");
+// var moviesArray = [];
+// var foodArray = ['hotdog','taco','pizza','icecream'];
+
+// form.addEventListener("submit", function(e) {
+//   e.preventDefault();
+//   createElements();
+// });
+
+// var createElements = function() {
+//   var output = document.querySelector(".output");
+//   var mainInput = document.querySelector(".main-input");
+//   var cardList = document.createElement("div");
+//   cardList.classList.add("card-list");
+//   output.appendChild(cardList);
+//   getMovieData(mainInput, output, cardList);
+// };
+
+// //GET food API & Display on page
+// var getMovieData = function(userInput, result, card) {
+//   var input = userInput;
+//   var movieResult = result;
+//   var newCard = card;
+//   $.ajax({
+//     type: "GET",
+//     url: `http://www.omdbapi.com/?apikey=24ec2260&t=${input.value}`,
+//     success: function(data) {
+//       moviesArray.push(data);
+//       moviesArray.forEach(function(movie) {
+//         if (movie.Genre === 'Comedy, Drama') {
+//           newCard.textContent = movie.Title;
+//           movieResult.appendChild(newCard);
+//           console.log(movie);
+//         } else {
+//           console.log(movie.Genre);
+//         }
+//       });
+//     },
+//     error: function(error) {
+//       console.log(error);
+//     }
+//   });
+// };
+
+// var getMoviesAPI = function() {
+//   $.ajax({
+//     type:'GET',
+//     url:`http://www.omdbapi.com/?apikey=24ec2260&t`,
+//     success: function(data) {
+//       moviesArray.push(data);
+//       moviesArray.forEach(function(movies){
+//         return movies;
+//       })
+//     },
+//     error: function(error) {
+//       console.log(error);
+//     }
+//   })
+// }
+
+// getMoviesAPI();
+
 var form = document.querySelector(".form");
 var moviesArray = [];
 var foodArray = ['hotdog','taco','pizza','icecream'];
@@ -57,3 +120,15 @@ var getMovieData = function(input, output, cardList) {
   })
 }
  getMoviesAPI();
+
+
+var dropDownShow = document.querySelector('.dropdown')
+
+var viewRecipeButton = document.querySelector('.viewRecipeButton')
+viewRecipeButton.addEventListener("click", function(x){
+  x.preventDefault();
+  console.log("yes")
+  dropDownShow.classList.remove('dropdownHidden');
+
+})
+
