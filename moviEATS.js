29e92aab45abca5dac6610a8ed66cb9cab4b55a8
TRACY S MUSIKER <tracymusiker@TRACYs-MacBook-Air.local>
2018-08-03 13:33:23 -0400

    removed comments

--- moviEATS.js
+++ moviEATS.js
@@ -1,8 +1,3 @@
-// if comedy = chicken
-// then genrefood = chicken
-
-
-
 var url = 'https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/autocomplete?number=10&query='
 
 $.ajax(url + "pasta", {
